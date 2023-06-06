b6962f1783be6469f7f2eb64e61e7475cd50d234
Daya Yadav <dayay@Dayas-MacBook-Air.local>
2023-06-06 15:58:46 +0530

    Configured SSL certification in Application Gateway

--- main.tf
+++ main.tf
@@ -10,68 +10,75 @@ provider "azurerm" {
   features {}
 }
 
+# Resource Group
 resource "azurerm_resource_group" "rg-daya-test-terraform" {
   name     = var.azure-resource
   location = var.resource-location
 }
 
 
-# Virtual Network
-resource "azurerm_virtual_network" "vn-daya-test-terraform" {
-  name                = "vn-dt-terraform"
-  resource_group_name = var.azure-resource
-  location            = var.resource-location
+
+# Virtual network
+resource "azurerm_virtual_network" "vnet-daya" {
+  name                = "vnet-daya-test"
   address_space       = ["10.0.0.0/16"]
-  tags = {
-    environment = "dev"
-  }
-}
-
-# Azure DNS Zone
-resource "azurerm_dns_zone" "zone-daya" {
-  name                = "daya.com"
-  resource_group_name = var.azure-resource
-}
-
-# Public IP
-resource "azurerm_public_ip" "pip" {
-  name                = "pip-daya-test"
-  resource_group_name = var.azure-resource
   location            = var.resource-location
-  allocation_method   = "Static"
-  ip_version          = "IPv4"
-  sku                 = "Standard"
-  sku_tier            = "Regional"
-  domain_name_label   = "daya-dns"
-}
-
-# DNS record
-resource "azurerm_dns_a_record" "dns-record" {
-  name                = "www"
   resource_group_name = var.azure-resource
-  zone_name           = azurerm_dns_zone.zone-daya.name
-  ttl                 = 1
-  target_resource_id  = azurerm_public_ip.pip.id
 }
 
-# Subnet 1
-resource "azurerm_subnet" "subnet-1" {
-  name                 = "vnet-sub-1"
+# Data Sources
+data "azurerm_client_config" "current" {}
+data "azurerm_key_vault" "kv-daya-test-4" {
+  name                = "kv-daya-test-4"
+  resource_group_name = var.azure-resource
+}
+# Key-vault Certificate
+data "azurerm_key_vault_certificate" "key-vault-certificate" {
+  name         = "kv-daya-test"
+  key_vault_id = data.azurerm_key_vault.kv-daya-test-4.id
+}
+
+
+# Key-Vault-access-policy
+resource "azurerm_key_vault_access_policy" "kv_access_policy" {
+  key_vault_id = data.azurerm_key_vault.kv-daya-test-4.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_user_assigned_identity.user_assigned.principal_id
+
+  key_permissions         = var.kv-key-permissions-full
+  secret_permissions      = var.kv-secret-permissions-full
+  storage_permissions     = var.kv-storage-permissions-full
+  certificate_permissions = var.kv-certificate-permissions-full
+}
+
+# Public IP  
+resource "azurerm_public_ip" "pip_ip" {
+  name                = "awf_public_ip"
+  location            = var.resource-location
+  resource_group_name = var.azure-resource
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# Subnet
+resource "azurerm_subnet" "sub-net" {
+  name                 = "subnet-daya-test"
   resource_group_name  = var.azure-resource
-  virtual_network_name = azurerm_virtual_network.vn-daya-test-terraform.name
+  virtual_network_name = azurerm_virtual_network.vnet-daya.name
   address_prefixes     = ["10.0.3.0/24"]
 }
-#Subnet 2 
-resource "azurerm_subnet" "subnet-2" {
-  name                 = "vnet-sub-2"
-  resource_group_name  = var.azure-resource
-  virtual_network_name = azurerm_virtual_network.vn-daya-test-terraform.name
-  address_prefixes     = ["10.0.4.0/24"]
+
+
+# User Assigned Identity
+resource "azurerm_user_assigned_identity" "user_assigned" {
+  name                = "daya-test-user"
+  resource_group_name = var.azure-resource
+  location            = var.resource-location
 }
 
 # Application Gateway 
-resource "azurerm_application_gateway" "awf-daya-test" {
-  name                = "appgatway"
+resource "azurerm_application_gateway" "app_gateway" {
+  name                = "myAppGateway"
   resource_group_name = var.azure-resource
   location            = var.resource-location
 
@@ -81,19 +88,27 @@ resource "azurerm_application_gateway" "awf-daya-test" {
     capacity = 2
   }
 
+  identity {
+    type = "UserAssigned"
+    identity_ids = [
+      azurerm_user_assigned_identity.user_assigned.id
+    ]
+  }
+
+
   gateway_ip_configuration {
     name      = "my-gateway-ip-configuration"
-    subnet_id = azurerm_subnet.subnet-1.id
+    subnet_id = azurerm_subnet.sub-net.id
   }
 
   frontend_port {
     name = var.frontend_port_name
-    port = 80
+    port = 443
   }
 
   frontend_ip_configuration {
     name                 = var.frontend_ip_configuration
-    public_ip_address_id = azurerm_public_ip.pip.id
+    public_ip_address_id = azurerm_public_ip.pip_ip.id
   }
 
   backend_address_pool {
@@ -103,70 +118,33 @@ resource "azurerm_application_gateway" "awf-daya-test" {
   backend_http_settings {
     name                  = var.http_setting_name
     cookie_based_affinity = "Disabled"
-    port                  = 80
-    protocol              = "Http"
-    request_timeout       = 60
+    port                  = 443
+    protocol              = "Https"
   }
 
   http_listener {
     name                           = var.listener_name
     frontend_ip_configuration_name = var.frontend_ip_configuration
     frontend_port_name             = var.frontend_port_name
-    protocol                       = "Http"
+    protocol                       = "Https"
+    ssl_certificate_name           = data.azurerm_key_vault_certificate.key-vault-certificate.name
+  }
+  ssl_certificate {
+    name                = data.azurerm_key_vault_certificate.key-vault-certificate.name
+    key_vault_secret_id = data.azurerm_key_vault_certificate.key-vault-certificate.versionless_secret_id
   }
 
   request_routing_rule {
     name                       = var.request_routing_rule_name
+    priority                   = 2
     rule_type                  = "Basic"
     http_listener_name         = var.listener_name
     backend_address_pool_name  = var.backend_address_pool_name
     backend_http_settings_name = var.http_setting_name
-    priority                   = 1
   }
+
+  depends_on = [
+    azurerm_key_vault_access_policy.kv_access_policy
+  ]
+
 }
-
-# Network interface
-resource "azurerm_network_interface" "network-interface" {
-  name                = "vm-ni-daya-test"
-  location            = var.resource-location
-  resource_group_name = var.azure-resource
-
-  ip_configuration {
-    name                          = "daya-test-config1"
-    subnet_id                     = azurerm_subnet.subnet-2.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-# Virtual Machine
-resource "azurerm_virtual_machine" "vm-daya-test" {
-  name                  = "vm-daya-test"
-  location              = var.resource-location
-  resource_group_name   = var.azure-resource
-  network_interface_ids = [azurerm_network_interface.network-interface.id]
-  vm_size               = "Standard_DS1_v2"
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = var.os-profile-computer-name
-    admin_username = var.os-profile-admin-username
-    admin_password = var.os-profile-admin-password
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-}
-
-
-
--- variables.tf
+++ variables.tf
@@ -27,6 +27,11 @@ variable "frontend_port_name" {
   sensitive   = true
   description = "front-end port name"
 }
+variable "frontend_port_name_ssl_cert" {
+  type        = string
+  sensitive   = true
+  description = "front-end port name for SSL"
+}
 variable "frontend_ip_configuration" {
   type        = string
   sensitive   = true
@@ -48,6 +53,12 @@ variable "listener_name" {
   sensitive   = true
   description = "listener name"
 }
+variable "listener_name_ssl" {
+  type        = string
+  default     = true
+  description = "SSL listener name"
+}
+
 variable "request_routing_rule_name" {
   type        = string
   sensitive   = true
@@ -71,5 +82,23 @@ variable "os-profile-admin-password" {
   description = "admin password"
 }
 
-
-
+variable "kv-key-permissions-full" {
+  type        = list(string)
+  description = "List of full key permissions, must be one or more from the following: backup, create, decrypt, delete, encrypt, get, import, list, purge, recover, restore, sign, unwrapKey, update, verify and wrapKey."
+  default     = ["Backup", "Create", "Decrypt", "Delete", "Encrypt", "Get", "Import", "List", "Purge", "Recover", "Restore", "Sign", "UnwrapKey", "Update", "Verify", "WrapKey"]
+}
+variable "kv-secret-permissions-full" {
+  type        = list(string)
+  description = "List of full secret permissions, must be one or more from the following: backup, delete, get, list, purge, recover, restore and set"
+  default     = ["Backup", "Delete", "Get", "List", "Purge", "Recover", "Restore", "Set"]
+}
+variable "kv-certificate-permissions-full" {
+  type        = list(string)
+  description = "List of full certificate permissions, must be one or more from the following: backup, create, delete, deleteissuers, get, getissuers, import, list, listissuers, managecontacts, manageissuers, purge, recover, restore, setissuers and update"
+  default     = ["Create", "Delete", "DeleteIssuers", "Get", "GetIssuers", "Import", "List", "ListIssuers", "ManageContacts", "ManageIssuers", "Purge", "Recover", "SetIssuers", "Update", "Backup", "Restore"]
+}
+variable "kv-storage-permissions-full" {
+  type        = list(string)
+  description = "List of full storage permissions, must be one or more from the following: backup, delete, deletesas, get, getsas, list, listsas, purge, recover, regeneratekey, restore, set, setsas and update"
+  default     = ["Backup", "Delete", "DeleteSAS", "Get", "GetSAS", "List", "ListSAS", "Purge", "Recover", "RegenerateKey", "Restore", "Set", "SetSAS", "Update"]
+}
